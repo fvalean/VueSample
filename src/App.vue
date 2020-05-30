592d44b1265ca0ee7ec9caf6bc4934f57a0da125
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-29 23:13:53 -0400

    Get data from api

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,30 @@
 </template>
 
 <script>
-import SearchBar from "./components/SearchBar";
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+const API_KEY = process.env.VUE_APP_API_KEY;
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    SearchBar
+    SearchBar,
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
-    }
-  }
+      console.log(API_KEY);
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then((response) => console.log(response));
+    },
+  },
 };
 </script>
 
