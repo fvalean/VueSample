d8284d542f9ffaa53af8b4b33ae4bb315cb0a293
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-31 23:28:09 -0400

    pass data to child component

---
+++ src/components/VideoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    Video list contains
+    {{
+      videos.length
+    }}
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  props: {
+    videos: Array,
+  },
+};
+</script>
+
+<style scoped></style>
